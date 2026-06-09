da7ac9ca3dd6e99a609bf824d00f0d33397b7950
manraf11 <168047318+manraf11@users.noreply.github.com>
2026-06-09 10:46:29 -0400

    Delete src directory

--- src/interfaces/I_vBioanalista.ts
+++
@@ -1,7 +0,0 @@
-import Cl_mExamen from "../models/Cl_mExamen.js";
-
-export interface I_vBioanalista {
-  cuandoCargarResultados(callback: (idExamen: string, resultados: string[]) => void): void;
-  cuandoFinalizarExamen(callback: (idExamen: string) => void): void;
-  mostrarPendientes(datos: { examenes: Cl_mExamen[] }): void;
-}
